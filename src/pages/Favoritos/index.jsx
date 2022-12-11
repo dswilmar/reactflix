0839b7572bfb5b88ae36cc5facc3135bab9ca368
Wilmar dos Santos <dswilmar@gmail.com>
2022-12-11 07:42:44 -0300

    feat: removendo filmes dos favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -11,9 +11,18 @@ const Favoritos = () => {
         setFilmes(JSON.parse(minhaLista) || []);
     }, []);
 
+    function excluirFilme(id) {
+        let filtroFilmes = filmes.filter((item) => {
+            return(item.id !== id)
+        });
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@reactflix", JSON.stringify(filtroFilmes));
+    }
+
     return (
         <div className="meus-filmes">
             <h1>Meus filmes</h1>
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo. &#128529;</span> }
             <ul>
                 {filmes.map((filme) => {
                     return (
@@ -21,7 +30,7 @@ const Favoritos = () => {
                             <span>{filme.title}</span>
                             <div>
                                 <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(filme.id)}>Excluir</button>
                             </div>
                         </li>
                     )
