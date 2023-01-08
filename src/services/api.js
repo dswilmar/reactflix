5f2fa2f5ed0689491b6467b3015c2e7e624b4570
Wilmar dos Santos <dswilmar@gmail.com>
2023-01-08 08:56:31 -0300

    hotfix: correcao na url base

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //movie/now_playing?api_key=13f587e8928001aad5c1f93409a6cf01&language=pt-br
 
 const api = axios.create({
-    baseURL: 'http://api.themoviedb.org/3/'
+    baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default api;
